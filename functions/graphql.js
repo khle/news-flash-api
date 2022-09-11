37caf2847cb407d75657c6db7f96605be4f3bc61
Kevin H Le <kevin.le@frosch.com>
2022-09-11 06:41:39 -0500

    replace hello world with news flash api

--- functions/graphql.js
+++ functions/graphql.js
@@ -1,19 +1,30 @@
-/*
 const { typeDefs, resolvers } = require('./bundle/schema');
 const { createLambdaServer } = require('./bundle/server');
 
-const apolloServer = createLambdaServer({
-  typeDefs,
-  resolvers,
-});
+const getHandler = (event, context) => {
+  const apolloServer = createLambdaServer({
+    typeDefs,
+    resolvers,
+    debug: true,
+  });
+  apolloServer.createHandler();
+  const graphqlHandler = apolloServer.createHandler();
+  if (!event.requestContext) {
+    event.requestContext = context;
+  }
+  return graphqlHandler(event, context);
+};
 
+exports.handler = getHandler;
+
+/*
 exports.handler = apolloServer.createHandler({
   cors: {
     origin: '*',
   },
 });
 */
-
+/*
 const { ApolloServer, gql } = require('apollo-server-lambda');
 
 const typeDefs = gql`
@@ -45,3 +56,4 @@ const getHandler = (event, context) => {
 };
 
 exports.handler = getHandler;
+*/
