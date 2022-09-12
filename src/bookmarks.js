8636227d5642b0830b8277abbf3abf101099550c
Kevin H Le <kevin.le@frosch.com>
2022-09-12 12:01:34 -0500

    fix a bug Expected Iterable, but did not find one for field Query.bookmarks. Need to return [] instead of null?

--- src/bookmarks.js
+++ src/bookmarks.js
@@ -2,7 +2,8 @@ const { getCollection, initDB, getDB } = require('lokijs-promise');
 
 const getBookmarks = async () => {
   const bookmarkCollection = await getCollection('bookmarks');
-  return bookmarkCollection.find({});
+  result = bookmarkCollection.find({});
+  return result ? result : [];
 };
 
 const setBookmarks = async bookmarks => {
