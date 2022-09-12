9dc3d59071e6cdcc82ba1b45db773f8cd85504b1
Kevin H Le <kevin.le@frosch.com>
2022-09-12 12:03:39 -0500

    run bundle fix a bug Expected Iterable, but did not find one for field Query.bookmarks. Need to return [] instead of null?

--- functions/bundle/bookmarks.js
+++ functions/bundle/bookmarks.js
@@ -1,36 +1,12 @@
 const { getCollection, initDB, getDB } = require('lokijs-promise');
 
 const getBookmarks = async () => {
-  /* const result = await new Promise((resolve, reject) => {
-    fs.readFile('./data.json', 'utf8', (error, data) => {
-      if (error) {
-        reject(error);
-      } else {
-        resolve(data);
-      }
-    });
-  }); 
-  return JSON.parse(result).bookmarks;
-  */
   const bookmarkCollection = await getCollection('bookmarks');
-
-  return bookmarkCollection;
+  result = bookmarkCollection.find({});
+  return result ? result : [];
 };
 
 const setBookmarks = async bookmarks => {
-  /* await new Promise((resolve, reject) => {
-    fs.writeFile(
-      './data.json',
-      JSON.stringify({ bookmarks }, null, 2),
-      error => {
-        if (error) {
-          reject(error);
-        } else {
-          resolve();
-        }
-      },
-    );
-  }); */
   const bookmarkCollection = await getCollection('bookmarks');
   bookmarkCollection.insert(bookmarks);
 };
