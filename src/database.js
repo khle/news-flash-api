df47de345fabe9d168f29e7f74bff19aa851cc3e
Kevin H Le <kevin.le@frosch.com>
2022-09-15 16:34:26 -0500

    add db.json

--- src/database.js
+++ src/database.js
@@ -1,15 +1,10 @@
 const { JsonDB, Config } = require('node-json-db');
-//const low = require('lowdb');
-//const FileSync = require('lowdb/adapters/FileSync');
 
 let db = undefined;
 
 function createDB() {
   if (!db) {
     db = new JsonDB(new Config('db.json', true, false, '/'));
-    //db = new Low(new JSONFile('file.json'));
-    //const adapter = new FileSync('db.json');
-    //db = low(adapter);
   }
   return db;
 }
