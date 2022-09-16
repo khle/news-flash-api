4e13763acdab140bd35adce88dd65d1de4fce968
Kevin H Le <kevin.le@frosch.com>
2022-09-16 13:53:44 -0500

    replace im mory db with node-json-db

--- functions/bundle/database.js
+++ functions/bundle/database.js
@@ -1,12 +1,12 @@
-//const { JsonDB, Config } = require('node-json-db');
-const { InMemoryDatabase } = require('in-memory-database');
+const { JsonDB, Config } = require('node-json-db');
+//const { InMemoryDatabase } = require('in-memory-database');
 
 let db = undefined;
 
 function createDB() {
   if (!db) {
-    //db = new JsonDB(new Config('db.json', true, false, '/'));
-    db = new InMemoryDatabase();
+    db = new JsonDB(new Config('db.json', true, false, '/'));
+    //db = new InMemoryDatabase();
   }
   return db;
 }
